d4ae238d55e0fa1168fb9718b266526d4771dd10
Shea Mpho <sheampho@gmail.com>
2022-12-11 06:30:26 -0800

    numberz

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,5 +1,5 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - returns alphabets both lower and upper cases
@@ -11,7 +11,7 @@ int main(void)
 	int ch = 'a';
 	int CH = 'A';
 
-	while (CH <= 'z')
+	while (ch <= 'z')
 	{
 		putchar(ch);
 		ch++;
