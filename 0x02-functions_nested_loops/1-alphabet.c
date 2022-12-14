212892adad67c733618265ed91e920c965591942
Shea Mpho <sheampho@gmail.com>
2022-12-13 23:56:00 -0800

    I sometimes suffer from insomnia

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabets in lowercase
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+	_putchar('\n');
+}
