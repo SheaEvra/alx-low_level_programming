e454068b9e47300bb13f930760b9ce90adec4596
Shea Mpho <sheampho@gmail.com>
2022-12-20 01:37:45 -0800

    strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,11 +9,11 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int count 0;
+	int count = 0;
 
-	while (count > 0)
+	while (count >= 0)
 	{
-		*(dest + count) = *(src = count);
+		*(dest + count) = *(src + count);
 		if (*(src + count) == '\0')
 			break;
 		count++;
