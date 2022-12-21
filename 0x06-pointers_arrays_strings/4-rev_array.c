cfe8f6057fc2cb6d90b3924793ffb8d30da59dc5
Shea Mpho <sheampho@gmail.com>
2022-12-21 06:22:32 -0800

    task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h";
+
+/**
+ * reverse_array - reverses the content of an array of integers
+ * @n: number of elements of the array
+ * @a: array
+ * Return: rev
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+	int x;
+
+	for (i = 0; i < n; i++)
+	{
+		n--;
+		j = a[i];
+		a[i] = a[n];
+		a[n] = x;
+	}
+}
